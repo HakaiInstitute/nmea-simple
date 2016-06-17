25336f637a3a0828e3cdecd10a727fa48286606f
Chris Davis <davis.christopher@gmail.com>
2016-06-17 13:48:32 -0700

    Create MTW.js

---
+++ codecs/MTW.js
@@ -0,0 +1,28 @@
+var helpers = require("../helpers.js")
+
+/*
+ === MTW - Mean Temperature of Water ===
+
+ ------------------------------------------------------------------------------
+        1   2 3
+        |   | |
+ $--MTW,x.x,C*hh<CR><LF>
+ ------------------------------------------------------------------------------
+
+ Field Number:
+ 1. Degrees
+ 2. Unit of Measurement, Celcius
+ 3. Checksum
+ */
+
+exports.TYPE = 'depth-transducer';
+exports.ID = 'DPT';
+
+exports.decode = function(fields) {
+  return {
+    sentence: exports.ID,
+    type: exports.TYPE,
+    waterTemp: +fields[1],
+    waterTempUnits: fields[2]
+  }
+}
